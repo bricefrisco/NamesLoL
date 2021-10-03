b8883de10e17709bf064985210243b147b097e1d
Brice <bricemfrisco@gmail.com>
2021-10-02 21:09:28 -0500

    User helper functions

--- src/functions/summoners-api/handler.ts
+++ src/functions/summoners-api/handler.ts
@@ -4,6 +4,8 @@ import {querySummoners, querySummonersByNameSize} from "@libs/dynamoDB";
 import {mapDynamoSummoner} from "@libs/mapper";
 import {SummonerEntity} from "@libs/types/summonerEntity";
 import {AttributeMap, QueryOutput} from "aws-sdk/clients/dynamodb";
+import {badRequest, error} from "@libs/responses";
+import {parseNameLength, parseTimestamp} from "@libs/validation";
 
 const regions: string[] = Object.values(Region).map((r) => r.toLowerCase())
 
@@ -16,61 +18,6 @@ const respond = (summoners: SummonerEntity[]): APIGatewayProxyResult => {
       backwards: summoners && summoners.length > 0 && summoners[0].availabilityDate
     })
   }
-}
-
-const badRequest = (error: string) => {
-  return {
-    statusCode: 400,
-    body: JSON.stringify({error})
-  }
-}
-
-const error = (error: string) => {
-  return {
-    statusCode: 400,
-    body: JSON.stringify({error})
-  }
-}
-
-const parseTimestamp = (timestamp: string): number => {
-  if (timestamp == null) {
-    throw new Error('Timestamp cannot be null. Example: /{region}/summoners?timestamp=12345')
-  }
-
-  let parsedTimestamp: number
-
-  try {
-    parsedTimestamp = parseInt(timestamp)
-  } catch (e) {
-    throw new Error('Could not parse timestamp. Please include numbers only. Example: /{region}/summoners?timestamp=12345')
-  }
-
-  if (parsedTimestamp < 1) {
-    throw new Error('Timestamp cannot be less than 1.')
-  }
-
-  return parsedTimestamp
-}
-
-const parseNameLength = (nameLength: string): number => {
-  if (nameLength == null) return null;
-
-  let parsedNameLength: number
-  try {
-    parsedNameLength = parseInt(nameLength)
-  } catch (e) {
-    throw new Error('Could not parse name length. Please include a number between 3 and 16')
-  }
-
-  if (parsedNameLength < 3) {
-    throw new Error('nameLength cannot be less than 3.')
-  }
-
-  if (parsedNameLength > 16) {
-    throw new Error('nameLength cannot be greater than 16.')
-  }
-
-  return parsedNameLength
 }
 
 export const main = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
